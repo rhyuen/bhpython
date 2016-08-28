a9ba5dacafe559fe395b75fb82d73d0a46b73c42
Robert Yuen <dungeonry@hotmail.com>
2016-08-28 06:22:37 -0700

    tcpserver and udpclient

---
+++ README.md
@@ -0,0 +1,7 @@
+### About
+
+Just some scripts in this book that I'm working through.  Putting it on github is a convenient way to have the code on hand for all your devices.
+
+###
+
+###
---
+++ udpclient.py
@@ -0,0 +1,10 @@
+import socket, sys
+
+target_host = "127.0.0.1"
+target_port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.sendto(bytes("IAMACAT", "utf-8"), (target_host, target_port))
+data, addr = client.recvfrom(4096)
+
+print(data)
